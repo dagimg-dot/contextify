e94bc0f825bd7f9deabe9b3f896c882a8fb25d8e
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-08-28 00:35:29 +0300

    chore: setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png, maskable-icon-512x512.png"],
+      manifest: {
+        name: "Contextify",
+        short_name: "Contextify",
+        description: "Word meaning without losing the context",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
