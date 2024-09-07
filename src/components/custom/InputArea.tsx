dc49f89a8feda67dd8e5de41fef84e03ebd3da05
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-09-07 14:12:56 +0300

    refactor: use popover instead of dialog for the prompt preview

--- src/components/custom/InputArea.tsx
+++ src/components/custom/InputArea.tsx
@@ -1,32 +1,33 @@
 import { useState, useCallback, useRef, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { Eye, Send } from "lucide-react";
+import { Send } from "lucide-react";
 import { db } from "@/services/db";
 import useGlobalStore from "@/store";
 import { toast } from "sonner";
 import { mergeInput } from "@/utils/inputMerger";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import { useAIQuery } from "@/services/api/useAPIQuery";
+import { useLiveQuery } from "dexie-react-hooks";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const InputArea = () => {
   const [input, setInput] = useState("");
   const [isPreviewing, setIsPreviewing] = useState(false);
   const [finalPrompt, setFinalPrompt] = useState("");
-  const { data, isLoading, error } = useAIQuery(finalPrompt);
+  const { data, error, refetch } = useAIQuery(finalPrompt);
   const textareaRef: React.RefObject<HTMLTextAreaElement> = useRef(null);
   const {
     currentConversationId: conversationId,
     isStreaming,
     setCurrentStreamingContent,
     setIsStreaming,
+    setIsLoading,
   } = useGlobalStore();
+
+  const messages = useLiveQuery(
+    () => db.messages.where("conversationId").equals(conversationId!).toArray(),
+    [conversationId]
+  );
 
   const adjustHeight = () => {
     if (textareaRef.current) {
@@ -34,6 +35,20 @@ const InputArea = () => {
       textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
     }
   };
+
+  useEffect(() => {
+    if (data) {
+      setCurrentStreamingContent(data);
+      console.log("data", data);
+    }
+  }, [data, setCurrentStreamingContent]);
+
+  useEffect(() => {
+    if (error) {
+      console.error("Error:", error);
+      toast.error(`Error: ${error.message}`);
+    }
+  }, [error]);
 
   useEffect(() => {
     adjustHeight();
@@ -86,24 +101,20 @@ const InputArea = () => {
       });
       setInput("");
       setIsStreaming(true);
+      setIsLoading(true);
 
-      // // Simulate AI response
-      // setTimeout(() => {
-      //   const aiResponse = finalPrompt;
-      //   setCurrentStreamingContent(aiResponse);
-      // }, 1000);
+      const result = await refetch();
 
-      if (data) {
-        setCurrentStreamingContent(data);
+      if (result.data) {
+        setCurrentStreamingContent(result.data);
+        console.log("data", result.data);
       }
 
-      if (error) {
-        toast.error("Error: " + error);
+      if (result.error) {
+        toast.error("Error: " + result.error);
       }
 
-      if (isLoading) {
-        toast.loading("Loading...");
-      }
+      setIsLoading(false);
     }
   }, [
     input,
@@ -111,68 +122,68 @@ const InputArea = () => {
     setCurrentStreamingContent,
     setIsStreaming,
     updateConversationTitle,
-    data,
-    error,
-    isLoading,
+    refetch,
+    setIsLoading,
   ]);
 
-  const handlePreview = useCallback(() => {
-    setIsPreviewing(true);
-  }, []);
+  useEffect(() => {
+    if (input.trim() !== "") {
+      setIsPreviewing(true);
+      textareaRef.current?.focus();
+    } else {
+      setIsPreviewing(false);
+    }
+  }, [input]);
 
   return (
-    <>
-      <footer className="p-4 border-t">
-        <div className="relative flex items-center">
-          <Textarea
-            ref={textareaRef}
-            placeholder="Enter a word and its context..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                handleSend();
-              }
-            }}
-            className="flex-1 min-h-[10px] h-[40px] max-h-60 transition-all duration-200 overflow-hidden pr-12"
-            style={{ paddingRight: "4rem" }} // Provide space for the buttons
-          />
-          <div className="absolute right-2 bottom-2 flex space-x-2">
-            <Button
-              onClick={handlePreview}
-              size="icon"
-              disabled={isStreaming || input.trim() === ""}
-            >
-              <Eye className="w-4 h-4" />{" "}
-              <span className="sr-only">Preview</span>
-            </Button>
-            <Button
-              onClick={handleSend}
-              size="icon"
-              disabled={isStreaming || input.trim() === ""}
-            >
-              <Send className="w-4 h-4" />
-              <span className="sr-only">Send</span>
-            </Button>
-          </div>
+    <footer className="p-4 border-t">
+      <div className="relative flex items-center">
+        <Popover open={isPreviewing}>
+          <PopoverTrigger asChild>
+            <div className="w-full">
+              <Textarea
+                ref={textareaRef}
+                placeholder={
+                  messages?.length > 0
+                    ? "Ask a follow-up question..."
+                    : "Enter a word and its context..."
+                }
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    handleSend();
+                  }
+                }}
+                className="flex-1 min-h-[10px] h-[40px] max-h-60 transition-all duration-200 overflow-hidden pr-12"
+              />
+            </div>
+          </PopoverTrigger>
+          <PopoverContent className="w-80 mb-4" side="top">
+            <div className="space-y-2">
+              <h4 className="font-medium leading-none">Preview</h4>
+              <p className="text-sm text-muted-foreground">
+                Here's how your input will be transformed into a prompt:
+              </p>
+              <div className="border p-2 rounded-md">
+                <p className="text-sm">{finalPrompt}</p>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
+        <div className="absolute right-2 bottom-2 flex space-x-2">
+          <Button
+            onClick={handleSend}
+            size="icon"
+            disabled={isStreaming || input.trim() === ""}
+          >
+            <Send className="w-4 h-4" />
+            <span className="sr-only">Send</span>
+          </Button>
         </div>
-      </footer>
-
-      <Dialog open={isPreviewing} onOpenChange={setIsPreviewing}>
-        <DialogContent className="max-w-[370px] rounded-md">
-          <DialogHeader>
-            <DialogTitle>Preview</DialogTitle>
-            <DialogDescription>
-              Here's how your input will be transformed into a prompt:
-            </DialogDescription>
-          </DialogHeader>
-          <div className="border p-2 rounded-md">
-            <p className="text-sm">{finalPrompt}</p>
-          </div>
-        </DialogContent>
-      </Dialog>
-    </>
+      </div>
+    </footer>
   );
 };
 
